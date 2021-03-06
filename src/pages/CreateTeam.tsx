21265541e4f0e2f4ec00587f2de347a1de121ac1
Flavio Eduardo Tapparo <tapparo.flavio@gmail.com>
2021-03-05 21:22:38 -0300

    Implemented the second part of 'Create your team' including the players cards and tactical schema but this don't have an functionality implemented.

--- src/pages/CreateTeam.tsx
+++ src/pages/CreateTeam.tsx
@@ -1,4 +1,7 @@
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/CreateTeam.css';
 
 export default function CreateTeam() {
@@ -54,12 +57,108 @@ export default function CreateTeam() {
                 <div className='configure-squad'>
                     <span>CONFIGURE SQUAD</span>
                     <div>
-                        <div>C</div>
-                        <div>D</div>
+                        {/* -------------- LEFT SECTION --------------- */}
+                        <div>
+                            <div className='formation-container'>
+                                <label htmlFor="formations">Formation</label>
+                                <select name="formations" id="formations">
+                                    <option value="choice1">4-2-3-1</option>
+                                    <option value="choice2">3-4-3</option>
+                                    <option value="choice3">4-5-1</option>
+                                    <option value="choice4">4-4-2</option>
+                                    <option value="choice5">4-3-3</option>
+                                    <option value="choice6">4-4-1-1</option>
+                                    <option value="choice7">3-3-1-3</option>
+                                    <option value="choice8">4-4-2</option>
+                                    <option value="choice9">3-5-2</option>
+                                </select>
+                                <div>
+                                    <span />
+                                    <FontAwesomeIcon icon={faSortDown} />
+                                </div>
+                            </div>
+                            <div className='tactical-schema'>
+                                <span></span>
+                            </div>
+                            <div className='button-save'>
+                                <Link to='/' >Save</Link>
+                            </div>
+                        </div>
+
+                        {/* -------------- RIGHT SECTION --------------- */}
+                        <div>
+                            <div className='search-players'>
+                                <span>Search Players</span>
+                                <input type='text' placeholder='Ronaldo' />
+                            </div>
+
+                            <div className='player-card'>
+                                <div>
+                                    <div>
+                                        <div>
+                                            <span>Name:</span>
+                                            <span>Cristiano Ronaldo</span>
+                                        </div>
+                                        <div>
+                                            <span>Age:</span>
+                                            <span>32</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <span>Nacionality:</span>
+                                            <span>Portugal</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='player-card'>
+                                <div>
+                                    <div>
+                                        <div>
+                                            <span>Name:</span>
+                                            <span>Cristiano Ronaldo</span>
+                                        </div>
+                                        <div>
+                                            <span>Age:</span>
+                                            <span>32</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <span>Nacionality:</span>
+                                            <span>Portugal</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='player-card'>
+                                <div>
+                                    <div>
+                                        <div>
+                                            <span>Name:</span>
+                                            <span>Cristiano Ronaldo</span>
+                                        </div>
+                                        <div>
+                                            <span>Age:</span>
+                                            <span>32</span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <span>Nacionality:</span>
+                                            <span>Portugal</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     );
 }
 
